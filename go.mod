3b30f44f49ff44f2ad51fda7a32feabe20f09685
zExiTeD <yknd80@gmail.com>
2024-08-06 22:35:40 +0530

    formted

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module zExiTeD/Hanashi
+module Hanashi/chat-app
 
 go 1.22.5
--- main.go
+++ main.go
@@ -2,27 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+func home(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+	fmt.Fprintf(w, "<H1>Hello There</H1>\n")
 }
 
 func main() {
+	
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
-	fmt.Printf("Server Started at Port :8090")
-	http.ListenAndServe(":8090", nil)
+	mux.HandleFunc("/",home)
+	
+	log.Println("\x1b[38;5;196m[LOG]\x1b[0m  Server Starting at Port :8080")
+	err := http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		fmt.Println("\x1b[38;5;196m[ERROR]\x1b[0m Failed to Start Server |")
+	}
+
 }
