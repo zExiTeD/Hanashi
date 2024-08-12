27a0437eb3ef2984d4a39721a52088a54d99db7c
zExiTeD <yknd80@gmail.com>
2024-08-12 20:55:53 +0530

    d

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module Hanashi/chat-app
 
 go 1.22.5
+
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	golang.org/x/net v0.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,27 +1,76 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"fmt"
+	"github.com/gorilla/websocket"
 )
 
-func home(w http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprintf(w, "<H1>Hello There</H1>\n")
+var Upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
-func main() {
-	
-	mux := http.NewServeMux()
+type hellos struct {
+	Hello string	`json:"text"`
+}
 
-	mux.HandleFunc("/",home)
-	
-	log.Println("\x1b[38;5;196m[LOG]\x1b[0m  Server Starting at Port :8080")
-	err := http.ListenAndServe(":8080", mux)
+func hello (w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.templ"))
+	data := hellos{
+		Hello : "hii",
+	} 
+	tmpl.Execute(w,data)
+}
 
+
+func WebSocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := Upgrader.Upgrade(w, r, nil)
+	fmt.Println(conn.LocalAddr())
 	if err != nil {
-		fmt.Println("\x1b[38;5;196m[ERROR]\x1b[0m Failed to Start Server |")
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	log.Println("Client connected")
+
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	
+		var MessAge hellos
+		err = json.Unmarshal(message,&MessAge)
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("Received message:", MessAge.Hello, messageType)
+		msg:=fmt.Sprintf(`<div id="chat" hx-swap-oob="beforeend"><p>message recived %s</p></div>`,MessAge.Hello)
+		err = conn.WriteMessage(websocket.TextMessage, []byte(msg))
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	}
 
 }
+
+func main() {
+	http.HandleFunc("/",hello)
+	http.HandleFunc("/ws", WebSocketHandler)
+
+	log.Print("Server starting at Port :8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("error server couldnt start")
+	}
+}
+
+
