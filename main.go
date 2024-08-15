416b5d5d6d934251d660180f8743b32bd967cd86
zExiTeD <yknd80@gmail.com>
2024-08-15 13:36:26 +0530

    broadcasting done

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 )
-
+var clients = make(map[*websocket.Conn]bool)
+var broadcast =make(chan string)
 var Upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -29,12 +30,15 @@ func hello (w http.ResponseWriter, r *http.Request) {
 
 func WebSocketHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := Upgrader.Upgrade(w, r, nil)
-	fmt.Println(conn.LocalAddr())
+	log.Println(conn.LocalAddr())
+
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	defer conn.Close()
+	clients[conn]=true
+
 	log.Println("Client connected")
 
 	for {
@@ -53,19 +57,34 @@ func WebSocketHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		log.Println("Received message:", MessAge.Hello, messageType)
 		msg:=fmt.Sprintf(`<div id="chat" hx-swap-oob="beforeend"><p>message recived %s</p></div>`,MessAge.Hello)
-		err = conn.WriteMessage(websocket.TextMessage, []byte(msg))
-		if err != nil {
-			log.Println(err)
-			return
-		}
+		broadcast <- msg
+//		err = conn.WriteMessage(websocket.TextMessage, []byte(msg))
+//		if err != nil {
+//			log.Println(err)
+//			return
+//		}
 	}
 
+}
+
+func handlemsg(){
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err:=client.WriteMessage(websocket.TextMessage,[]byte(msg))
+			if err!= nil {
+				client.Close()
+				log.Println("failed to write message")
+			}
+		}
+	}
 }
 
 func main() {
 	http.HandleFunc("/",hello)
 	http.HandleFunc("/ws", WebSocketHandler)
-
+	
+	go handlemsg()
 	log.Print("Server starting at Port :8080")
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
